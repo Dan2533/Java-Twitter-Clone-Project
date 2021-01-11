cf92669534ff4f8c95382ba062e41f68c2fe8f04
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-01-11 18:30:10 -0500

    Added Spring Boot

---
+++ src/main/java/com/codedifferently/blather/BlatherApplication.java
@@ -0,0 +1,13 @@
+package com.codedifferently.blather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codedifferently/blather/BlatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedifferently.blather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
